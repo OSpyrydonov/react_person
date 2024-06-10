4dc69f9eab0d272903e6dd083a49a15fb0467ba2
Oleksandr Spyrydonov <spiridonovsasa731@gmail.com>
2024-06-10 20:41:19 +0300

    correcred code based on second commets

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,25 +4,20 @@ const FEMALE = 'f';
 const PARTNER_ROLE = {
   [MALE]: 'husband',
   [FEMALE]: 'wife',
-}
+};
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partnerRole = sex === MALE
-   ? PARTNER_ROLE[FEMALE]
-   : PARTNER_ROLE[MALE];
+  const partnerRole = sex === MALE ? PARTNER_ROLE[FEMALE] : PARTNER_ROLE[MALE];
+  const maritalStatus = isMarried
+    ? `${partnerName} is my ${partnerRole}`
+    : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
-      {isMarried ? (
-        <p className="Person__partner">
-          {`${partnerName} is my ${partnerRole}`}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">{maritalStatus}</p>
     </section>
   );
 };
