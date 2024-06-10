9aef01a903fbd5c22cf17696c7bd1f764bf7c0cf
Oleksandr Spyrydonov <spiridonovsasa731@gmail.com>
2024-06-10 13:25:05 +0300

    corrected code based on comments

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,28 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {person.sex === 'm'
-          ? `${person.partnerName} is my wife`
-          : `${person.partnerName} is my husband`}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married </p>
-    )}
-  </section>
-);
+const MALE = 'm';
+const FEMALE = 'f';
+
+const PARTNER_ROLE = {
+  [MALE]: 'husband',
+  [FEMALE]: 'wife',
+}
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partnerRole = sex === MALE
+   ? PARTNER_ROLE[FEMALE]
+   : PARTNER_ROLE[MALE];
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      {isMarried ? (
+        <p className="Person__partner">
+          {`${partnerName} is my ${partnerRole}`}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
